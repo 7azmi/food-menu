78fdcd3073d7ec6cb8bd740576f4959e63659bea
7azmi <a7.h3.04@gmail.com>
2023-12-27 04:21:59 +0800

    Assignment 2 code uploaded

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 #include <fstream>
 #include <sstream>
+#include <string>
 
 class MenuItem {
 public:
@@ -10,16 +9,33 @@ public:
     std::string category;
     double price;
 
-    MenuItem(std::string name, std::string category, double price) : name(name), category(category), price(price) {
+    MenuItem(std::string name = "", std::string category = "", double price = 0.0) : name(name), category(category), price(price) {}
+};
 
-    }
+class MenuItemNode {
+public:
+    MenuItem data;
+    MenuItemNode* next;
+
+    MenuItemNode(MenuItem data) : data(data), next(nullptr) {}
 };
 
 class FoodOrderingSystem {
 private:
-    std::vector<MenuItem> menu;
+    MenuItemNode* head;
 
 public:
+    FoodOrderingSystem() : head(nullptr) {}
+
+    ~FoodOrderingSystem() {
+        MenuItemNode* current = head;
+        while (current != nullptr) {
+            MenuItemNode* next = current->next;
+            delete current;
+            current = next;
+        }
+    }
+
     void loadMenuItems(const std::string& filename) {
         std::ifstream file(filename);
         std::string line, name, category;
@@ -30,99 +46,113 @@ public:
             std::getline(ss, name, ',');
             std::getline(ss, category, ',');
             ss >> price;
-            menu.push_back(MenuItem(name, category, price));
+            addMenuItem(MenuItem(name, category, price));
         }
     }
 
-    void printMenu() const {
-        for (const auto& item : menu) {
-            std::cout << "Name: " << item.name << ", Category: " << item.category << ", Price: $" << item.price << std::endl;
+    void addMenuItem(const MenuItem& item) {
+        MenuItemNode* newNode = new MenuItemNode(item);
+        if (!head) {
+            head = newNode;
+        } else {
+            MenuItemNode* current = head;
+            while (current->next) {
+                current = current->next;
+            }
+            current->next = newNode;
         }
     }
 
+    void deleteMenuItem(const std::string& name) {
+        MenuItemNode* current = head;
+        MenuItemNode* prev = nullptr;
 
-    void sortMenuByName() {
-        std::sort(menu.begin(), menu.end(), [](const MenuItem& a, const MenuItem& b) {
-            return a.name < b.name;
-        });
+        while (current != nullptr) {
+            if (current->data.name == name) {
+                if (prev == nullptr) {
+                    head = current->next;
+                } else {
+                    prev->next = current->next;
+                }
+                delete current;
+                return;
+            }
+            prev = current;
+            current = current->next;
+        }
+        std::cout << "Item not found: " << name << std::endl;
     }
 
-    void sortMenuByPrice() {
-        std::sort(menu.begin(), menu.end(), [](const MenuItem& a, const MenuItem& b) {
-            return a.price < b.price;
-        });
-    }
-
-    void searchMenuByName(const std::string& searchName) {
+    void searchMenuItem(const std::string& searchName) {
+        MenuItemNode* current = head;
         bool found = false;
-        for (const auto& item : menu) {
-            if (item.name.find(searchName) != std::string::npos) {
-                std::cout << "Found: " << item.name << ", Category: " << item.category << ", Price: $" << item.price << std::endl;
+        while (current != nullptr) {
+            if (current->data.name.find(searchName) != std::string::npos) {
+                std::cout << "Found: " << current->data.name << ", Category: " << current->data.category << ", Price: $" << current->data.price << std::endl;
                 found = true;
             }
+            current = current->next;
         }
         if (!found) {
             std::cout << "No items found with name: " << searchName << std::endl;
         }
     }
 
-    void searchMenuByCategory(const std::string& searchCategory) {
-        bool found = false;
-        for (const auto& item : menu) {
-            if (item.category == searchCategory) {
-                std::cout << "Found: " << item.name << ", Category: " << item.category << ", Price: $" << item.price << std::endl;
-                found = true;
-            }
-        }
-        if (!found) {
-            std::cout << "No items found in category: " << searchCategory << std::endl;
+    void printMenu() const {
+        MenuItemNode* current = head;
+        while (current != nullptr) {
+            std::cout << "Name: " << current->data.name << ", Category: " << current->data.category << ", Price: $" << current->data.price << std::endl;
+            current = current->next;
         }
     }
-
 };
+
 
 
 int main() {
     FoodOrderingSystem system;
-    system.loadMenuItems("menu.txt"); // Load menu items from the file
+
+    system.loadMenuItems("menu.txt");
 
     int choice;
-    std::string input;
+    std::string inputName, inputCategory;
+    double inputPrice;
 
     do {
         std::cout << "\nFood Ordering System Menu\n";
-        std::cout << "1. Print Menu\n";
-        std::cout << "2. Sort Menu by Name\n";
-        std::cout << "3. Sort Menu by Price\n";
-        std::cout << "4. Search Menu by Name\n";
-        std::cout << "5. Search Menu by Category\n";
+        std::cout << "1. Add Menu Item\n";
+        std::cout << "2. Delete Menu Item\n";
+        std::cout << "3. Search Menu by Name\n";
+        std::cout << "4. Print Menu\n";
         std::cout << "0. Exit\n";
         std::cout << "Enter your choice: ";
         std::cin >> choice;
 
         switch (choice) {
             case 1:
-                system.printMenu();
+                std::cout << "Enter name: ";
+                std::cin.ignore();
+                std::getline(std::cin, inputName);
+                std::cout << "Enter category: ";
+                std::getline(std::cin, inputCategory);
+                std::cout << "Enter price: ";
+                std::cin >> inputPrice;
+                system.addMenuItem(MenuItem(inputName, inputCategory, inputPrice));
                 break;
             case 2:
-                system.sortMenuByName();
-                system.printMenu();
+                std::cout << "Enter name of item to delete: ";
+                std::cin.ignore();
+                std::getline(std::cin, inputName);
+                system.deleteMenuItem(inputName);
                 break;
             case 3:
-                system.sortMenuByPrice();
-                system.printMenu();
+                std::cout << "Enter name to search: ";
+                std::cin.ignore();
+                std::getline(std::cin, inputName);
+                system.searchMenuItem(inputName);
                 break;
             case 4:
-                std::cout << "Enter name to search: ";
-                std::cin.ignore(); // Clear newline left in the buffer
-                std::getline(std::cin, input);
-                system.searchMenuByName(input);
-                break;
-            case 5:
-                std::cout << "Enter category to search: ";
-                std::cin.ignore(); // Clear newline left in the buffer
-                std::getline(std::cin, input);
-                system.searchMenuByCategory(input);
+                system.printMenu();
                 break;
             case 0:
                 std::cout << "Exiting the program.\n";
